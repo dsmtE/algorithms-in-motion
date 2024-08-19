6ec9a836a5cad3f2d68e6a2f85f4e57d4dc00fe0
dsmtE <desmet.enguerrand@gmail.com>
2024-08-19 20:09:08 +0200

    ✨ [merge_sort] recursivePhase V2

--- src/scenes/mergeSort/recursiveMergeSort.tsx
+++ src/scenes/mergeSort/recursiveMergeSort.tsx
@@ -1,11 +1,11 @@
-import { Circle, FlexContent, FlexItems, Layout, Line, makeScene2D } from "@motion-canvas/2d";
-import { Rect, Txt, Node} from "@motion-canvas/2d/lib/components";
+import { Layout, makeScene2D, View2D } from "@motion-canvas/2d";
+import { Rect, Txt} from "@motion-canvas/2d/lib/components";
 import { all, sequence, waitFor } from "@motion-canvas/core/lib/flow";
-import { createRefArray, makeRef, range, Reference, ReferenceArray, useRandom, useScene } from "@motion-canvas/core/lib/utils";
-import {Color, createRef, createSignal, Spacing} from '@motion-canvas/core';
+import { makeRef, range, Reference, useRandom, useScene } from "@motion-canvas/core/lib/utils";
+import {Color, createRef, ThreadGenerator} from '@motion-canvas/core';
 import { Colors, textStyle } from "@/styles/styles";
 import {Style as ArrayStyle, boxWidthGap} from "@/utils/ArrayConstants";
-import { sorted_permutation, reverse_index_mapping, zip } from "@/utils/utils";
+import { sorted_permutation, reverse_index_mapping } from "@/utils/utils";
 
 const AS = {...ArrayStyle};
 AS.boxWidth /= 1.2;
@@ -64,6 +64,112 @@ function create_splitted_container(
         ];
 }
 
+function* sort_array(
+    view: View2D,
+    total_levels: number,
+    current_size: number,
+    current_container: Reference<Layout>,
+    current_outline: Rect,
+    current_rects: Rect[],
+    current_level: number = 0,
+    previous_highlighted: Rect | null = null,
+) : ThreadGenerator {
+    // Do nothing if size is 0 or 1
+    if (current_size <= 1) {
+        yield current_outline.stroke(Colors.green, 0.5);
+        yield* waitFor(0.5);
+        return;
+    }
+
+    yield previous_highlighted?.save();
+    yield previous_highlighted?.stroke((previous_highlighted.stroke() as Color).alpha(0.3), 0.3);
+    yield current_outline.stroke(Colors.blue, 0.3);
+    
+    let current_half_size = Math.floor(current_size/2);
+
+    const left_container = createRef<Layout>();
+    const left_outline = createRef<Rect>();
+
+    const right_container = createRef<Layout>();
+    const right_outline = createRef<Rect>();
+
+    view.add(create_splitted_container(
+        current_container,
+        current_half_size,
+        current_size-current_half_size,
+        left_container,
+        right_container,
+        4*AS.outlineMargin,
+        (Math.pow(total_levels-current_level, 2)+1)*AS.outlineMargin,
+    ))
+    view.add([
+        create_outline(left_container, left_outline, Colors.surface, AS.outlineMargin, 0),
+        create_outline(right_container, right_outline, Colors.surface, AS.outlineMargin, 0)
+    ]);
+    
+    left_container().y(left_container().y()+50),
+    right_container().y(right_container().y()+50),
+
+    yield* all(
+        left_container().y(left_container().y()-50, 0.3),
+        right_container().y(right_container().y()-50, 0.3),
+        left_outline().opacity(1, 0.3),
+        right_outline().opacity(1, 0.3),
+    )
+
+    // Move rects
+    yield* all(
+        sequence(0.1, ...current_rects.slice(0, current_half_size).map((rect, i) => 
+                rect.topLeft(pos_topleft_within(left_container, i, boxWidthGap(AS)), 0.2)
+        )),
+        sequence(0.1, ...current_rects.slice(current_half_size).map((rect, i) => 
+                rect.topLeft(pos_topleft_within(right_container, i, boxWidthGap(AS)), 0.2)
+        )),
+    );
+
+    yield* sort_array(
+        view,
+        total_levels,
+        current_half_size,
+        left_container,
+        left_outline(),
+        current_rects.slice(0, current_half_size),
+        current_level+1,
+        current_outline,
+    );
+    yield* sort_array(
+        view,
+        total_levels,
+        current_size - current_half_size,
+        right_container,
+        right_outline(),
+        current_rects.slice(current_half_size),
+        current_level+1,
+        current_outline,
+);
+    
+    const sorting_mapping = reverse_index_mapping(sorted_permutation(current_rects.map(rect => parseInt(rect.childAs<Txt>(0).text(), 10))));
+    // merge
+    // swap rect references
+    {
+    const tempRects = current_rects.slice();
+    sorting_mapping.forEach((targetIndex, i) => current_rects[targetIndex] = tempRects[i] );
+    }
+    
+    yield* sequence(0.2, ...current_rects.map((rect, i) => rect.topLeft(pos_topleft_within(current_container, i, boxWidthGap(AS)), 0.3)))
+    yield* all(
+        left_outline().opacity(0, 0.3),
+        right_outline().opacity(0, 0.3),
+    )
+    yield* waitFor(0.2);
+    
+    yield current_outline.stroke(Colors.green, 0.3);
+    yield previous_highlighted?.restore(0.3);
+    yield* waitFor(0.5);
+
+    [left_container, right_container, left_outline, right_outline].forEach(ref => ref().remove());
+}
+
 export default makeScene2D(function* (view) {
     const seed = useScene().variables.get('seed', 42);
     const size = useScene().variables.get('size', 7)();
@@ -116,105 +222,16 @@ export default makeScene2D(function* (view) {
         ))
     )
 
-    let container_to_split = [{current_size: size, current_container: main_container, current_level: 0, current_rects: rects}];
-    let container_to_merge : {
-        parent_container: Reference<Layout>,
-        left_outline: Reference<Layout>,
-        right_outline: Reference<Layout>,
-        rects: Rect[],
-    }[] = [];
-
     const total_levels = Math.ceil(Math.log2(size));
     
-    while(container_to_split.length > 0) {
-
-        const {current_size, current_container, current_level, current_rects} = container_to_split.pop();
-
-        if (current_size <= 1) {
-            continue;
-        }
-
-        let current_half_size = Math.floor(current_size/2);
-
-        const left_container = createRef<Layout>();
-        const left_outline = createRef<Rect>();
-    
-        const right_container = createRef<Layout>();
-        const right_outline = createRef<Rect>();
-
-        view.add(create_splitted_container(
-            current_container,
-            current_half_size,
-            current_size-current_half_size,
-            left_container,
-            right_container,
-            4*AS.outlineMargin,
-            (Math.pow(total_levels-current_level, 2)+1)*AS.outlineMargin,
-        ))
-        view.add([
-            create_outline(left_container, left_outline, Colors.surface, AS.outlineMargin, 0),
-            create_outline(right_container, right_outline, Colors.surface, AS.outlineMargin, 0)
-        ]);
-        
-        left_container().y(left_container().y()+50),
-        right_container().y(right_container().y()+50),
-
-        yield* all(
-            left_container().y(left_container().y()-50, 0.3),
-            right_container().y(right_container().y()-50, 0.3),
-            left_outline().opacity(1, 0.3),
-            right_outline().opacity(1, 0.3),
-        )
-
-        // Move rects
-        yield* all(
-            sequence(0.1, ...current_rects.slice(0, current_half_size).map((rect, i) => 
-                    rect.topLeft(pos_topleft_within(left_container, i, boxWidthGap(AS)), 0.2)
-            )),
-            sequence(0.1, ...current_rects.slice(current_half_size).map((rect, i) => 
-                    rect.topLeft(pos_topleft_within(right_container, i, boxWidthGap(AS)), 0.2)
-            )),
-        );
-
-        container_to_merge.push({
-            parent_container: current_container,
-            left_outline: left_outline,
-            right_outline: right_outline,
-            rects: current_rects,
-        });
-
-        container_to_split.push({
-            current_size: current_half_size,
-            current_container: left_container,
-            current_level: current_level+1,
-            current_rects: current_rects.slice(0, current_half_size)
-        });
-        container_to_split.push({
-            current_size: current_size - current_half_size,
-            current_container: right_container,
-            current_level: current_level+1,
-            current_rects: current_rects.slice(current_half_size)
-        });
-    }
-
-    while (container_to_merge.length > 0) {
-        const {parent_container, left_outline, right_outline, rects} = container_to_merge.pop();
-
-        const sorting_mapping = reverse_index_mapping(sorted_permutation(rects.map(rect => parseInt(rect.childAs<Txt>(0).text(), 10))));
-        // merge
-        // swap rect references
-        {
-        const tempRects = rects.slice();
-        sorting_mapping.forEach((targetIndex, i) => rects[targetIndex] = tempRects[i] );
-        }
-
-        yield* sequence(0.2, ...rects.map((rect, i) => rect.topLeft(pos_topleft_within(parent_container, i, boxWidthGap(AS)), 0.3)))
-        yield* all(
-            left_outline().opacity(0, 0.3),
-            right_outline().opacity(0, 0.3),
-        )
-        yield* waitFor(0.5);
-    }
+    yield* sort_array(
+        view,
+        total_levels,
+        size,
+        main_container,
+        main_outline(),
+        rects
+    );
 
     // color green
     yield* sequence(0.1, ...rects.map(rect => rect.fill(Colors.green, 0.3)))
