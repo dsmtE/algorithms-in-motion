fb6fbd2f94b0aa7e2191c40a0665060a53b69322
dsmtE <desmet.enguerrand@gmail.com>
2025-03-02 12:40:48 +0100

    ⬆️🐛 Fix bug updating motion-canvas 3.17.0 -> 3.17.2 (removing ffmpeg)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from 'vite';
 import motionCanvas from '@motion-canvas/vite-plugin';
-import ffmpeg from '@motion-canvas/ffmpeg';
+// import ffmpeg from '@motion-canvas/ffmpeg';
 import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
@@ -12,7 +12,7 @@ export default defineConfig({
         './src/projects/mergeSort.ts',
       ]
     }),
-    ffmpeg(),
+    // ffmpeg(),
   ],
   build: {
     rollupOptions: {
