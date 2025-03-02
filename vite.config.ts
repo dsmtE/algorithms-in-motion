7b935382c5264e92701b8618c8d06d73d3264107
dsmtE <desmet.enguerrand@gmail.com>
2025-03-02 12:43:11 +0100

    👷 Use fs to get projects list instead of hard-coded paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,39 @@ import {defineConfig} from 'vite';
 import motionCanvas from '@motion-canvas/vite-plugin';
 // import ffmpeg from '@motion-canvas/ffmpeg';
 import { fileURLToPath, URL } from 'url';
+import fs from 'fs';
 
-export default defineConfig({
-  plugins: [
-    motionCanvas({
-      project: [
-        './src/projects/bubbleSort.ts',
-        './src/projects/selectionSort.ts',
-        './src/projects/mergeSort.ts',
-      ]
-    }),
-    // ffmpeg(),
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        dir: './build',
-        entryFileNames: '[name].js',
+export default defineConfig(({ command, mode }) => {
+  let projectPath = './src/projects';
+  let projects = fs.readdirSync(projectPath);
+  // filter keep only file with .ts extension
+  projects = projects.filter(file => file.endsWith('.ts'));
+  // get absolute path
+  projects = projects.map(file => `${projectPath}/${file}`);
+
+  if(mode === 'development') {
+    console.log('projects:', projects);
+  }
+
+  return {
+    plugins: [
+      motionCanvas({
+        project: projects,
+      }),
+      // ffmpeg(),
+    ],
+    build: {
+      rollupOptions: {
+        output: {
+          dir: './build',
+          entryFileNames: '[name].js',
+        },
       },
     },
-  },
-  resolve: {
-    alias: [
-      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-    ],
-  },
+    resolve: {
+      alias: [
+        { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      ],
+    },
+  };
 });
